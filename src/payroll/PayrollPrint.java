bdd3247b554db79db0c45671d34f77d864aa2c32
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2020-06-24 14:09:31 -0400

    Update PayrollPrint.java

--- src/payroll/PayrollPrint.java
+++ src/payroll/PayrollPrint.java
@@ -15,14 +15,16 @@ public class PayrollPrint
 {
 public static void main(String[] args) 
 {
-/* here assumed that 50 employee accounts can be created
- for the given bank */
-Payroll ac[]=new Payroll[50];
+/* here assumed that 5 employee accounts can be created for the given bank */
+Payroll ac[]=new Payroll[5];
+ //add code to take input from user, If salary is less than or equal to 3000,calculate a new salary with bonus 
+ //and store in array then print 5 employee details
+  
 Scanner sc=new Scanner(System.in);
 System.out.println("Please enter the employee name:");
-//add code
+ 
 System.out.println("Please enter the salary: salary <=3000");
-       
+     
 System.out.println("The final Salary including bonus is: ");         
 
 }
