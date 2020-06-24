69454837a143dbf2e75b73b1b0fd4574ae87a5fb
singjova <singjova@sheridancollege.ca>
2020-06-24 16:34:05 -0400

    DOne

--- src/payroll/Payroll.java
+++ src/payroll/Payroll.java
@@ -10,25 +10,38 @@ package payroll;
  *  If salary is less than or equal to 3000,calculate a new salary with bonus.
  @author srinivsi
  */
-public class Payroll 
+public class Payroll
 {
 
-   public double Salary;
-   public double bonus;
-  public String emp;
- 
-     /**constructor that takes the salary and employee name
-      * 
-      */
-   public Payroll(double Sal,String givenUser)
-        {
-            Salary=Sal;
-            emp=givenUser;
-                 
+    public double Salary;
+    public double bonus;
+    public String emp;
+
+    /**constructor that takes the salary and employee name
+     *
+     */
+    public Payroll(double Sal,String givenUser)
+    {
+        Salary=Sal;
+        emp=givenUser;
+
+    }
+    public void newSalary(double salary){
+        if (salary<=3000){
+            double bonus=salary/10;
+            salary=salary+bonus;
+                    
+        
+            System.out.println("new Salary including bonus is " + salary+" and bonus is 10%");
         }
-      
-     
-    
-    
-    
+        else {
+            System.out.println("You income is more than 3000 therefore your salary including bonus is " +salary);
+        }
 }
+
+
+
+
+
+}
+
--- src/payroll/PayrollPrint.java
+++ src/payroll/PayrollPrint.java
@@ -11,22 +11,24 @@ import java.util.Scanner;
  * printing final salary including bonus
  * @author srinivsi
  */
-public class PayrollPrint 
+public class PayrollPrint
 {
-public static void main(String[] args) 
-{
-/* here assumed that 5 employee accounts can be created for the given bank */
-Payroll ac[]=new Payroll[5];
- //add code to take input from user, If salary is less than or equal to 3000,calculate a new salary with bonus 10% of salary
- //and store in array then print 5 employee details
-  
-Scanner sc=new Scanner(System.in);
-System.out.println("Please enter the employee name:");
- 
-System.out.println("Please enter the salary: salary <=3000");
-     
-System.out.println("The final Salary including bonus is: ");         
+    public static void main(String[] args)
+    {
+        /* here assumed that 5 employee accounts can be created for the given bank */
+        Payroll jovan=new Payroll(12.12,"jovan");
+        //add code to take input from user, If salary is less than or equal to 3000,calculate a new salary with bonus 10% of salary
+        //and store in array then print 5 employee details
+        double salary;
+        String employee;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter the employee name:");
+        employee=sc.nextLine();
+        System.out.println("Please enter the salary in decimal notation:");
+        salary=sc.nextDouble();
+        System.out.println("Hi " + employee + ", Your salary is displayed below");
+        jovan.newSalary(salary);
 
-}
+    }
 
 }
